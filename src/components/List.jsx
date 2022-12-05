79eb4c26f37223368a522cea8a9efcc77d4fa666
Ioan Mert Aydogan <77471284+ioanaydgn@users.noreply.github.com>
2022-12-05 21:21:30 +0200

    Initial commit

---
+++ src/components/List.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function List() {
+  return (
+    <ul>
+      <li>Becon</li>
+      <li>Jamon</li>
+      <li>Noodles</li>
+    </ul>
+  );
+}
+
+export default List;
